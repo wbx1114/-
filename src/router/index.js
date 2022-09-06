78303d881294f38b5733633d0daac9307c2f0dd0
ice <2190753551@qq.com>
2022-09-06 18:13:51 +0800

    feat:登录页完成一半

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,10 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/login',
+    component: () => import('@/views/login')
+  }
 ]
 
 const router = new VueRouter({
---
+++ src/views/login/index.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <!-- 头部导航 -->
+    <van-nav-bar title="登录" class="nav-bar" />
+    <!-- 登录表单 -->
+    <van-form @submit="onSubmit" class="from">
+      <van-field
+        v-model="mobile"
+        name="mobile"
+        placeholder="请输入手机号"
+        :rules="[
+          { required: true, message: '请填写手机号' },
+          {
+            pattern:
+              /^(?:(?:\+|00)86)?1(?:(?:3[\d])|(?:4[5-79])|(?:5[0-35-9])|(?:6[5-7])|(?:7[0-8])|(?:8[\d])|(?:9[189]))\d{8}$/,
+            message: '手机号格式错误'
+          }
+        ]"
+      >
+        <template #label>
+          <span class="toutiao toutiao-shouji"></span>
+        </template>
+      </van-field>
+      <van-field
+        v-model="code"
+        placeholder="请输入验证码"
+        :rules="[
+          { required: true, message: '请输入验证码' },
+          { pattern: /[0-9]{6}/, message: '验证码格式错误' }
+        ]"
+      >
+        <template #label>
+          <span class="toutiao toutiao-yanzhengma"></span>
+        </template>
+      </van-field>
+      <div style="margin: 16px">
+        <van-button block type="info" native-type="submit">登录</van-button>
+      </div>
+    </van-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      mobile: '',
+      code: ''
+    }
+  },
+  methods: {
+    onSubmit(values) {
+      console.log('submit', values)
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+.nav-bar {
+  background-color: #3296fa;
+
+  :deep(.van-nav-bar__title) {
+    color: #fff;
+  }
+}
+:deep(.from) {
+  .van-cell__title {
+    flex: 1;
+  }
+  .van-cell__value {
+    flex: 20;
+  }
+  .toutiao {
+    font-size: 40px;
+  }
+}
+</style>
